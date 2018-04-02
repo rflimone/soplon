4bfd700cd8cef3d6f38230dbb18ed8d22ad96b7f
rflimone <rflimone@PC-RenzoLimone>
2018-04-02 00:45:18 -0300

    Se mueve capa de negocio a donde corresponde

--- soplon/src/Soplon/Soplon.java
+++ soplon/src/Soplon/Soplon.java
@@ -1,27 +1,52 @@
 package Soplon;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-
 /**
  * A simple example, used on the jsoup website.
  */
 public class Soplon {
-    public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://readms.net/").get();
-        log(doc.title());
+   public static void main(String[] args) {
+        /*
+        Validate.isTrue(args.length == 1, "www.mangastream.com");
+        String url = "https://readms.net/manga/";
+        print("Fetching %s...", url);
 
+        Document doc = Jsoup.connect(url).get();
         Elements links = doc.select("a[href]");
-        for (Element l : links) {
-            log("%s\n\t%s", l.attr("title"), l.absUrl("href"));
+        Elements media = doc.select("[src]");
+        Elements imports = doc.select("link[href]");
+
+        print("\nMedia: (%d)", media.size());
+        for (Element src : media) {
+            if (src.tagName().equals("img"))
+                print(" * %s: <%s> %sx%s (%s)",
+                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
+                        trim(src.attr("alt"), 20));
+            else
+                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
+        }
+
+        print("\nImports: (%d)", imports.size());
+        for (Element link : imports) {
+            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
+        }
+
+        print("\nLinks: (%d)", links.size());
+        for (Element link : links) {
+            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
         }
     }
 
-    private static void log(String msg, String... vals) {
-        System.out.println(String.format(msg, vals));
+    private static void print(String msg, Object... args) {
+        System.out.println(String.format(msg, args));
     }
-}
+
+    private static String trim(String s, int width) {
+        if (s.length() > width)
+            return s.substring(0, width-1) + ".";
+        else
+            return s;
+    }
+    
+    */
+   } 
+ }   
