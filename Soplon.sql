0d77d695f7c19c7d7827ea26f3de706e0e49ace7
rflimone <37988344+rflimone@users.noreply.github.com>
2018-05-21 21:48:24 -0400

    cambio en BD
    se modifica unique key de categorias y se modifican columnas de paginas

--- Soplon.sql
+++ Soplon.sql
@@ -12,7 +12,7 @@ MySQL - 10.2.14-MariaDB : Database - mariadb_soplon
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`mariadb_soplon` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mariadb_soplon` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `mariadb_soplon`;
 
@@ -30,12 +30,12 @@ CREATE TABLE `categorias` (
 /*Data for the table `categorias` */
 
 insert  into `categorias`(`id_categorias`,`glosa_categoria`) values 
-(6,'Animé'),
-(4,'Cine'),
-(3,'Conciertos'),
+(1,'Mangas'),
 (2,'Deportes'),
+(3,'Conciertos'),
+(4,'Cine'),
 (5,'Libros'),
-(1,'Mangas');
+(6,'Animé');
 
 /*Table structure for table `metodos_envio` */
 
@@ -63,9 +63,9 @@ DROP TABLE IF EXISTS `paginas`;
 
 CREATE TABLE `paginas` (
   `id_paginas` int(11) NOT NULL AUTO_INCREMENT,
-  `dominio` varchar(255) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   `url_ultimo` varchar(255) DEFAULT NULL,
+  `titulo_pagina` varchar(255) DEFAULT NULL,
   `glosa_pagina` varchar(255) DEFAULT NULL,
   `date_last` datetime DEFAULT NULL,
   `date_new` datetime DEFAULT NULL,
@@ -81,11 +81,11 @@ CREATE TABLE `paginas` (
 
 /*Data for the table `paginas` */
 
-insert  into `paginas`(`id_paginas`,`dominio`,`url`,`url_ultimo`,`glosa_pagina`,`date_last`,`date_new`,`imagen`,`id_categorias`) values 
-(1,'readms.net','https://readms.net/','actualizado','MangaStream','2018-03-30 18:09:22','2018-03-30 18:09:22',NULL,1),
-(2,'readms.net','https://readms.net/','actualizado','MangaStream','2018-03-30 18:09:22','2018-03-30 18:09:22',NULL,1),
-(3,'jaiminisbox.com','https://jaiminisbox.com','actualizado','Jaiminisbox','2018-05-03 16:53:39','2018-05-10 16:53:48',NULL,1),
-(4,'ticketpro.cl','http://www.ticketpro.cl/sports/','actualizado','Ticketpro','2018-05-04 17:00:13','2018-05-11 17:00:26',NULL,2);
+insert  into `paginas`(`id_paginas`,`url`,`url_ultimo`,`titulo_pagina`,`glosa_pagina`,`date_last`,`date_new`,`imagen`,`id_categorias`) values 
+(1,'https://readms.net','https://readms.net/manga/one_piece','readms.net','MangaStream','2018-05-22 01:35:19','2018-03-30 18:09:22',NULL,1),
+(2,'https://readms.net/rss','http://readms.net/r/akagami/96/5067/1','Akagami no Shirayukihime 96','-no title-','2018-05-03 01:14:57','2018-05-12 15:27:26',NULL,1),
+(3,'https://jaiminisbox.com','https://jaiminisbox.com/reader/series/one-piece-2','jaiminisbox.com','Jaiminisbox','2018-05-22 01:35:20','2018-05-10 16:53:48',NULL,1),
+(4,'http://www.ticketpro.cl/rss/','http://www.ticketpro.cl/en/a92945-concerts/2310147-Gran-Arena-Monticello-Kool-And-The-Gang-Fiesta-Radio-Activa.html','KOOL AND THE GANG - FIESTA RADIO ACTIVA / GRAN ARENA MONTICELLO','Gran Arena Monticello, Sábado 29 de Septiembre / 21:00 hrs.','2018-05-03 01:14:57','2018-05-16 15:27:26',NULL,2);
 
 /*Table structure for table `subscripciones` */
 
