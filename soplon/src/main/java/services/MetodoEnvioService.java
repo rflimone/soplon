7eb440c89ca3375c4210397325a7afd19e313901
rflimone <rflimone@gmail.com>
2018-05-22 22:51:46 -0400

    Se crea metodo de envio de mails y su tester.

--- soplon/src/main/java/services/MetodoEnvioService.java
+++ soplon/src/main/java/services/MetodoEnvioService.java
@@ -2,7 +2,12 @@ package services;
 
 import dao.MetodoEnvioDao;
 import entities.MetodoEnvio;
+import java.io.IOException;
 import java.util.List;
+import org.simplejavamail.email.Email;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+import org.simplejavamail.mailer.config.TransportStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
@@ -19,6 +24,31 @@ public class MetodoEnvioService {
     @Transactional(readOnly = true)
     public List<MetodoEnvio> getAllMetodoEnvio() {
         return metodoEnvioDao.getAllMetodoEnvio();
+    }
+
+    public void SendMails(String user, String userMail, String tag, String titulo_pagina, String url_actualizada, String glosa_pagina, String date_new) throws IOException {
+         try {
+
+            Email email = EmailBuilder.startingBlank()
+                    .from("Soplon: Notificaciones de Entretencion", "soplonne@gmail.com")
+                    .to(user, userMail)
+                    .withSubject("Nuevo contenido de "+tag)
+                    .withPlainText("Enhorabuena!, lo nuevo de "+titulo_pagina+" ya esta diponible en "+url_actualizada)
+                    .withPlainText("En detalle, se trata de"+glosa_pagina)
+                    .withPlainText("Esto fue subido al sitio en fecha "+date_new)
+                    .withPlainText("Esperamos disfrutes de este contenido")
+                    .buildEmail();
+
+            MailerBuilder
+                    .withSMTPServer("smtp.gmail.com", 587, "soplonne@gmail.com", "soplonne123")
+                    .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                    //.withDebugLogging(true)
+                    .buildMailer()
+                    .sendMail(email);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
---
+++ soplon/src/test/java/api/SimpleMailTester.java
@@ -0,0 +1,43 @@
+package api;
+
+import org.simplejavamail.email.Email;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.simplejavamail.mailer.config.TransportStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Configuration
+public class SimpleMailTester {
+    
+    @Autowired 
+    public static void main(String[] args) {
+        
+        /**
+         * Revidar como gatillar el envio automatico a usuarios dependiendo de su seleccion de metodo de envio. 
+         * Se debe realizar el envio cuando un campo al cual el este subscrito sea actualizado o a;adido
+         */
+        
+        try {
+
+            Email email = EmailBuilder.startingBlank()
+                    .from("Soplon: Notificaciones de Entretencion", "soplonne@gmail.com")
+                    .to("Renzo Limone", "rflimone@gmail.com")
+                    .withSubject("Soplon test mail")
+                    .withPlainText("test test test")
+                    .buildEmail();
+
+            MailerBuilder
+                    .withSMTPServer("smtp.gmail.com", 587, "soplonne@gmail.com", "soplonne123")
+                    .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                    .withDebugLogging(true)
+                    .buildMailer()
+                    .sendMail(email);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+}
