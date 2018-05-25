635170147a2019aefa4f7b912537542880054fe1
Francisco Venegas López <fvenegas@cibergestion.cl>
2018-05-25 15:15:14 -0400

    Se cambian datos de acceso a base de datos en frontend

--- soplon-web/application/config/config.php
+++ soplon-web/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/soplon/soplon-web/';
+$config['base_url'] = 'http://192.155.83.79/';
 $config['name_app'] = 'Soplón';
 /*
 |--------------------------------------------------------------------------
